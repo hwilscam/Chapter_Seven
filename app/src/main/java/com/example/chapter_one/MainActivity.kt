596552fb164cf99311e900e504fd979466358b42
Hunter <hw55s@missouristate.edu>
2025-02-02 16:05:03 -0600

    Exercise 2 Working

--- app/src/main/java/com/example/chapter_one/MainActivity.kt
+++ app/src/main/java/com/example/chapter_one/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.chapter_one
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,19 +25,19 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener {
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                it,
                 R.string.correct_toast,
-                Toast.LENGTH_LONG
-            )
+                Snackbar.LENGTH_LONG
+                )
                 .show()
         }
 
         falseButton.setOnClickListener {
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                it,
                 R.string.incorrect_toast,
-                Toast.LENGTH_LONG
+                Snackbar.LENGTH_LONG
             )
                 .show()
         }
