d186a9e6558b378ec4bd258629271b5e4e09b383
Hunter <hw55s@missouristate.edu>
2025-02-09 13:07:58 -0600

    Chapter Two Update

--- app/src/main/java/com/example/chapter_two/MainActivity.kt
+++ app/src/main/java/com/example/chapter_two/MainActivity.kt
@@ -2,46 +2,52 @@ package com.example.chapter_two
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import com.example.chapter_two.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton : Button
     private lateinit var falseButton : Button
 
+    private lateinit var binding:ActivityMainBinding
+
     private val questionBank = listOf(
 
-    Question(R.string.question_australia, true),
-    Question(R.string.question_oceans, true),
-    Question(R.string.question_mideast, false),
-    Question(R.string.question_africa, false),
-    Question(R.string.question_americas, true),
-    Question(R.string.question_asia, true)
+    question(R.string.question_australia, true),
+    question(R.string.question_oceans, true),
+    question(R.string.question_mideast, false),
+    question(R.string.question_africa, false),
+    question(R.string.question_americas, true),
+    question(R.string.question_asia, true)
+
     )
 
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        // trueButton = findViewById(R.id.true_button)
+        // falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener {
-            Snackbar.make(
-                it,
-                R.string.correct_toast,
-                Snackbar.LENGTH_LONG
-                )
+        binding.trueButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Correct",
+                Toast.LENGTH_LONG
+            )
                 .show()
         }
 
-        falseButton.setOnClickListener {
-            Snackbar.make(
-                it,
-                R.string.incorrect_toast,
-                Snackbar.LENGTH_LONG
+        binding.falseButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Incorrect",
+                Toast.LENGTH_LONG
             )
                 .show()
         }
